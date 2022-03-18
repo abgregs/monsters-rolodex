5252e9828df8fc5556120ed1daf10543acc68c1d
Austin Gregersen <abgregs@gmail.com>
2022-03-18 12:31:43 -0600

    convert remaining components to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,12 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-  render() {
-    const { users } = this.props;
-
-    return (
-      <div className='card-list'>
-        {users.map((user) => {
-          return <Card user={user} key={user.id} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ users }) => (
+  <div className='card-list'>
+    {users.map((user) => {
+      return <Card user={user} key={user.id} />;
+    })}
+  </div>
+);
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,25 +1,21 @@
-import { Component } from 'react';
 import './card.styles.css';
 
-class Card extends Component {
-  render() {
-    const { name, email, id } = this.props.user;
-
-    return (
-      <div className='card-container'>
-        <div className='card-section'>
-          <img
-            alt={`user ${name}`}
-            src={`https://robohash.org/${id}?set=set3&size=180x180`}
-          />
-        </div>
-        <div className='card-section'>
-          <h2>{name}</h2>
-          <p>{email}</p>
-        </div>
+const Card = ({ user }) => {
+  const { name, email, id } = user;
+  return (
+    <div className='card-container'>
+      <div className='card-section'>
+        <img
+          alt={`user ${name}`}
+          src={`https://robohash.org/${id}?set=set3&size=180x180`}
+        />
       </div>
-    );
-  }
-}
+      <div className='card-section'>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
